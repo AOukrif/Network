68747a65bebeb2e8de9c2a7d56190362557fa770
aoukrif <abderaoufoukrif@gmail.com>
2025-08-26 13:41:24 +0100

    git init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+Ping pingSender = new Ping();
+
+PingOptions options = new PingOptions();
+options.DontFragment = true;
+
+string data = " Data to send ";
+byte[] buffer = Encoding.UTF8.GetBytes(data);
+
+int timeOut = 120;
+string addressToPing = "8.8.8.8";
+
+PingReply reply = pingSender.Send(addressToPing,timeOut,buffer,options);
+
+
+if(reply.Status == IPStatus.Success)
+{
+    Console.WriteLine($"Response: {reply.Status}");
+    Console.WriteLine($"RounfTrip: {reply.RoundtripTime}");
+    Console.WriteLine($"Time To Live: {reply.Options.Ttl}");
+    Console.WriteLine($"Buffer size: {buffer.Length}");
+}
+
+
+
+
+
