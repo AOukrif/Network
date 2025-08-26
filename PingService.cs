d3b0dc0a3807f862f91f026dffd935b27d13a8d9
aoukrif <abderaoufoukrif@gmail.com>
2025-08-26 14:30:58 +0100

    Refacto : Adding pingService

---
+++ PingService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    internal class PingService
+    {
+        public string Data { get; set; }
+        public string Address { get; set; }
+        public int TimeOut { get; set; }
+        public byte []  Buffer { get; set; }
+        public Ping pingSender  { get; set; }
+        public PingReply pingReply { get; set; }
+        public PingOptions pingOptions { get; set; }
+
+        public PingService(string data , string address , int timeOut)
+        {
+            this.Data = data;
+            this.Address = address;
+            this.TimeOut = timeOut;
+            this.Buffer = Encoding.UTF8.GetBytes(data);
+            this.pingSender = new Ping();
+            this.pingOptions = new PingOptions(); 
+        }
+
+        public void setOption()
+        {
+            pingOptions.DontFragment = true;
+        }
+
+        public void Reply()
+        {
+            pingReply=pingSender.Send(Address, TimeOut,Buffer,pingOptions);
+        }
+
+        public void CheckResult()
+        {
+            if (pingReply.Status == IPStatus.Success)
+            {
+                Console.WriteLine($"Response: {pingReply.Status}");
+                Console.WriteLine($"RounfTrip: {pingReply.RoundtripTime}");
+                Console.WriteLine($"Time To Live: {pingReply.Options.Ttl}");
+                Console.WriteLine($"Buffer size: {Buffer.Length}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
-﻿using System.Net.NetworkInformation;
+﻿using Network;
+using System.Net.NetworkInformation;
 using System.Text;
 
-Ping pingSender = new Ping();
-
-PingOptions options = new PingOptions();
-options.DontFragment = true;
-
 string data = " Data to send ";
-byte[] buffer = Encoding.UTF8.GetBytes(data);
-
 int timeOut = 120;
 string addressToPing = "8.8.8.8";
 
-PingReply reply = pingSender.Send(addressToPing,timeOut,buffer,options);
+PingService service = new PingService(data, addressToPing, timeOut);
+service.setOption();
+service.Reply();
+service.CheckResult();
 
 
-if(reply.Status == IPStatus.Success)
-{
-    Console.WriteLine($"Response: {reply.Status}");
-    Console.WriteLine($"RounfTrip: {reply.RoundtripTime}");
-    Console.WriteLine($"Time To Live: {reply.Options.Ttl}");
-    Console.WriteLine($"Buffer size: {buffer.Length}");
-}
+
 
 
 
